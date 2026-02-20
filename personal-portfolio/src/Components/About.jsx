16099019b9e3f5b4a97f747beca5ae7c40e252b9
ThandoJ <jasopthando0@gmail.com>
2026-02-20 21:18:06 +0200

    Added the about section

---
+++ personal-portfolio/src/Components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function About() {
+  return (
+    <section id="about" className="py-24 px-6 max-w-4xl mx-auto text-center">
+      <h2 className="text-4xl font-bold mb-6">About Me</h2>
+
+      <p className="text-lg text-gray-600 dark:text-gray-300 leading-relaxed">
+        I am a passionate developer focused on building responsive and user-friendly
+        web applications using modern technologies like React, JavaScript, HTML, and CSS. I'm an individual who is not afraid to get out of my comfort zone and eager to learn new things.
+      </p>
+    </section>
+  )
+}
+
+export default About
