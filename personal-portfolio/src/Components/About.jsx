793962dfae258a6ff495656aa6b5cfc0fc4375d0
Thando Jasop <jasopthando0@gmail.com>
2026-02-23 20:04:20 +0200

    Upgraded the contact section

--- personal-portfolio/src/Components/About.jsx
+++ personal-portfolio/src/Components/About.jsx
@@ -9,6 +9,10 @@ function About() {
           I'm a passionate frontend developer focused on crafting
           beautiful and functional user experiences.
           I enjoy solving complex problems and building scalable applications.
+          When I'm not coding, you can find me exploring the outdoors, sketching, or spending time with friends and family.
+          I'm a very flexable individual when it comes to my capabilies. I visualise myself being an independent and self-motivated young women who is always looking for new challenges and opportunities to grow. 
+          I'm a team player who enjoys collaborating with others and sharing knowledge. I'm also a strong communicator who can effectively convey ideas and concepts to both technical and non-technical audiences.
+          I'm always excited to connect with like-minded individuals and collaborate on innovative projects. Let's create something amazing together!
         </p>
       </div>
     </section>
